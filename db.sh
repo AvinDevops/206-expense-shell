7efdf28265d1e126b1dba071da95d3f88702813b
AvinDevops <avinashbandela73@gmail.com>
2025-11-04 20:07:19 +0530

    db

--- db.sh
+++ db.sh
@@ -10,15 +10,43 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-echo "$LOGFILE"
+
+#prompt for root password
+echo "please enter password"
+read mysql_root_password
 
 #checking root user or not
 if [ $USERID -ne 0 ]
 then
-    echo "please access with root user"
+    echo -e "$R please access with root user $N"
     exit 1
 else
-    echo "you are root user"
+    echo -e "$G you are root user $N"
 fi
 
-dnf install mysql-server -y
+# function validation
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is...$G SUCCESS $N"
+    fi
+}
+
+# installing mysql server
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+
+#enabling mysql
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Enabiling mysql"
+
+# starting mysql server
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "Starting mysql server"
+
+# setting password for root
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$LOGFILE
+VALIDATE $? "Setting root password"
